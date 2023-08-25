9109e14dab014e535a5239982dd8a5f0d6a1849b
superbisonzlat <andrey-osinkin@mail.ru>
2023-08-25 16:41:17 +0500

    Init2

--- src/Solution.java
+++ src/Solution.java
@@ -2,5 +2,6 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Ex");
+        System.out.println("4334");
     }
 }
